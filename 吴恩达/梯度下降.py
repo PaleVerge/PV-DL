8c2ca972d602b0f84e03404a86f3116b371ace68
PaleVerge <fnme370679@gmail.com>
2026-08-11 00:28:22 +0800

    notebook

---
+++ 吴恩达/梯度下降.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def compute(w,x,b):
+    m=len(x)
+    f_wb=np.zeros(m)
+    for i in range(m):
+        f_wb[i]=w*x[i]+b
+
+    return f_wb
+
+def cost_function(w,x,b):
+    m = len(x)
+    sum = 0
+    for i in range(m):
+        sum += compute(w,x,b)
+    j=sum/(2*m)
+    return j
+
+w=0
+w=w-a*
