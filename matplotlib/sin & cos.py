c36ede9fed45f9ec6c7af73872bbb41f4587306c
PaleVerge <fnme370679@gmail.com>
2026-08-11 00:06:29 +0800

    增加注释

--- matplotlib/sin & cos.py
+++ matplotlib/sin & cos.py
@@ -1,17 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.lines import lineStyles
 
-x=np.arange(-5,5,0.1)
-y1=np.sin(x)
-y2=np.cos(x)
+x = np.arange(-5,5,0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
 
-plt.plot(x,y1,label="sin")
+plt.plot(x,y1,label="sin") ## 图例标签
 plt.plot(x,y2,ls=":",label="cos")
 
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Sin & Cos Function")
+plt.xlabel("x") ## x轴标签
+plt.ylabel("y") ## y轴标签
+plt.title("Sin & Cos Function") ## 标题
 
-plt.legend()
-plt.show()
+plt.legend() ## 显示图例
+plt.show() ## 绘图
